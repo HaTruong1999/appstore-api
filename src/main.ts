da4d15837736007f6cb036426c9b9aa071c36673
DESKTOP-CU7PFRA\VNPT <havantruong@vnpt.vn>
2022-10-11 10:54:52 +0700

    cmt SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+//import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  const config = new DocumentBuilder()
-    .setTitle('APP STORE APIs')
-    .setDescription('The App Store API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('APP STORE APIs')
+  //   .setDescription('The App Store API description')
+  //   .setVersion('1.0')
+  //   .addBearerAuth()
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   // Kiểm tra đầu vào(pipes)
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
