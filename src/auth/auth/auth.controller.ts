58cbeda45ba037bd87a116293155ae6ad559ac89
Ha Truong <hatruong2628@gmail.com>
2022-10-24 23:21:04 +0700

    upload avt 1

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, Get, Post, UseGuards,Request, Res, Req, UseInterceptors, HttpException, HttpStatus, UploadedFile } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request, Res, Req, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { ChangePasswordDto } from './dto/changepassword.dto';
 import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
@@ -7,11 +7,6 @@ import { Response } from 'express';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
 import { AvatarDto, UsersDto } from 'src/users/users/dto/users.dto';
 import { FileInterceptor } from '@nestjs/platform-express/multer';
-// import { extname } from 'path';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const multer  = require('multer');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const FileType = require('file-type');
 import { diskStorage } from 'multer';
 import { editFileName, imageFileFilter } from 'src/ultils/file-upload.utils';
 // const MAX_SIZE = 5242880; //5MB
@@ -20,123 +15,122 @@ import { editFileName, imageFileFilter } from 'src/ultils/file-upload.utils';
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
-    @Post('login')
-    @ApiBody({ type: AuthDto })
-    async login(@Body() authDto: AuthDto) {
-        return this.authService.sign(authDto);
+  @Post('login')
+  @ApiBody({ type: AuthDto })
+  async login(@Body() authDto: AuthDto) {
+    return this.authService.sign(authDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('checkToken')
+  async checkToken(@Request() req: any) {
+    return this.authService.checkToken(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('changePassword')
+  @ApiBody({ type: ChangePasswordDto })
+  async changePassword(
+    @Body() change: ChangePasswordDto,
+    @Res() res: Response,
+  ) {
+    const result = await this.authService.changePassword(change);
+    if (result.code === 1) {
+      return res.status(201).json({
+        ...result,
+        success: true,
+      });
+    } else {
+      return res.status(200).json({
+        success: false,
+        ...result,
+      });
     }
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('checkToken')
-    async checkToken(@Request() req: any) {
-        return this.authService.checkToken(req.user);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('account')
+  async account(
+    @Req() req: any
+  ) {
+    return this.authService.findOne(req.user.userId)
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post('changePassword')
-    @ApiBody({ type: ChangePasswordDto })
-    async changePassword(
-        @Body() change: ChangePasswordDto,
-        @Res() res: Response,
-    ) {
-        const result = await this.authService.changePassword(change);
-        if(result.code === 1){
-            return res.status(201).json({
-                ...result,
-                success: true,
-            });
-        }else{
-            return res.status(200).json({
-                success: false,
-                ...result,
-            });
-        }
-    }
+  //Cập nhật thông tin
+  @UseGuards(JwtAuthGuard)
+  @Post('/changeProfile')
+  update(@Body() updateUsersDto: UsersDto, @Req() req: any) {
+    return this.authService.update(
+      req.user.userId,
+      updateUsersDto
+    );
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('account')
-    async account(
-      @Req() req: any
-    ) {
-      return this.authService.findOne(req.user.userId)
-    }
+  // @UseGuards(JwtAuthGuard)
+  // @UseInterceptors(
+  //   FileInterceptor('file', {
+  //     storage: multer.diskStorage({
+  //         destination: './uploads/avatars',
+  //         filename: function (req, file, cb) {
+  //           const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+  //           cb(null, file.fieldname + '-' + uniqueSuffix);
+  //           //avtSrc = file.fieldname + '-' + uniqueSuffix;
+  //         }
+  //       }),
+  //     // storage: multer.memoryStorage(),
+  //     // dest: './uploads/avatars',
+  //     // limits: {
+  //     //   fileSize: MAX_SIZE,
+  //     // },
+  //     // fileFilter: (req: any, file: any, cb: any) => {
+  //     //   if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+  //     //     // Allow storage of file
+  //     //     cb(null, true);
+  //     //   } else {
+  //     //     // Reject file
+  //     //     cb(
+  //     //       new HttpException(
+  //     //         `Không hỗ trợ định dạng của file ${extname(file.originalname)}`,
+  //     //         HttpStatus.BAD_REQUEST,
+  //     //       ),
+  //     //       false,
+  //     //     );
+  //     //   }
+  //     // },
+  //   }),
+  // )
+  // @UseInterceptors(FileInterceptor('file'))
+  // @Post('changeAvatar')
+  // async uploadFile(
+  //   @Body() body: AvatarDto,
+  //   @UploadedFile() file,
+  //   @Req() req: any
+  // ) {
+  //   const ex = await FileType.fromBuffer(file.buffer);
+  //   if (!ex.mime.match(/\/(jpg|jpeg|png|gif)$/)) {
+  //     throw new HttpException(`Không hỗ trợ định dạng của file ${ex.ext}`, HttpStatus.BAD_REQUEST);
+  //   }
+  //   body.avatarID = req.user.userId;
+  //   return this.authService.upload(body, file);
+  // }
 
-    //Cập nhật thông tin
-    @UseGuards(JwtAuthGuard)
-    @Post('/changeProfile')
-    update(@Body() updateUsersDto: UsersDto, @Req() req: any) {
-      return this.authService.update(
-        req.user.userId,
-        updateUsersDto
-      );
-    }
-
-    // @UseGuards(JwtAuthGuard)
-    // @UseInterceptors(
-    //   FileInterceptor('file', {
-    //     storage: multer.diskStorage({
-    //         destination: './uploads/avatars',
-    //         filename: function (req, file, cb) {
-    //           const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    //           cb(null, file.fieldname + '-' + uniqueSuffix);
-    //           //avtSrc = file.fieldname + '-' + uniqueSuffix;
-    //         }
-    //       }),
-    //     // storage: multer.memoryStorage(),
-    //     // dest: './uploads/avatars',
-    //     // limits: {
-    //     //   fileSize: MAX_SIZE,
-    //     // },
-    //     // fileFilter: (req: any, file: any, cb: any) => {
-    //     //   if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
-    //     //     // Allow storage of file
-    //     //     cb(null, true);
-    //     //   } else {
-    //     //     // Reject file
-    //     //     cb(
-    //     //       new HttpException(
-    //     //         `Không hỗ trợ định dạng của file ${extname(file.originalname)}`,
-    //     //         HttpStatus.BAD_REQUEST,
-    //     //       ),
-    //     //       false,
-    //     //     );
-    //     //   }
-    //     // },
-    //   }),
-    // )
-    // @UseInterceptors(FileInterceptor('file'))
-    // @Post('changeAvatar')
-    // async uploadFile(
-    //   @Body() body: AvatarDto,
-    //   @UploadedFile() file,
-    //   @Req() req: any
-    // ) {
-    //   const ex = await FileType.fromBuffer(file.buffer);
-    //   if (!ex.mime.match(/\/(jpg|jpeg|png|gif)$/)) {
-    //     throw new HttpException(`Không hỗ trợ định dạng của file ${ex.ext}`, HttpStatus.BAD_REQUEST);
-    //   }
-    //   body.avatarID = req.user.userId;
-    //   return this.authService.upload(body, file);
-    // }
-
-    @Post('changeAvatar')
-    @UseInterceptors(
-        FileInterceptor('file', {
-        storage: diskStorage({
-            destination: './uploads/avatars',
-            filename: editFileName,
-        }),
-        fileFilter: imageFileFilter,
-        }),
-    )
-    async uploadedFile(@UploadedFile() file) {
-        const response = {
-          originalname: file.originalname,
-          filename: file.filename,
-        };
-        return response;
-      }
+  @Post('changeAvatar')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/avatars',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadedFile(
+    @Body() body: AvatarDto,
+    @UploadedFile() file,
+  ) {
+    return this.authService.upload(body, file);
+  }
 
 }
--- src/auth/auth/auth.service.ts
+++ src/auth/auth/auth.service.ts
@@ -5,265 +5,239 @@ import { Users } from 'src/common/entities/users.entity';
 import { UsersService } from 'src/users/users/users.service';
 import { ChangePasswordDto } from './dto/changepassword.dto';
 import { Like, Repository } from 'typeorm';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const multer  = require('multer')
 import { AvatarDto, UsersDto } from 'src/users/users/dto/users.dto';
+const AVT_PATH = 'uploads/avatars/';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { promisify } = require('util')
+
+const unlinkAsync = promisify(fs.unlink)
 // import path = require('path');
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService : UsersService,
-        private jwtService: JwtService,
-        @InjectRepository(Users)
-        private usersRepository: Repository<Users>
-    ){}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>
+  ) { }
 
-    async validateUser(userCode: string, pass: string): Promise<any> {
-        // const user = await this.usersService.findUserByEmail(username);
-        const user = await this.usersRepository.findOne({
-          where: [
-            { userEmail : Like(userCode) },
-            { userCode : Like(userCode) },
-          ]
-        });
-    
-        if (user && user.userPassword === pass) {
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const { userPassword, ...result } = user;
-          return result;
-        }
-        return null;
+  async validateUser(userCode: string, pass: string): Promise<any> {
+    // const user = await this.usersService.findUserByEmail(username);
+    const user = await this.usersRepository.findOne({
+      where: [
+        { userEmail: Like(userCode) },
+        { userCode: Like(userCode) },
+      ]
+    });
+
+    if (user && user.userPassword === pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { userPassword, ...result } = user;
+      return result;
     }
-    
-    async sign(userDto: any) {
-      try {
-        const user = await this.validateUser(userDto.username, userDto.password);
-        if (user === null){
-          return {
-            code: 0,
-            data:{
-              accessToken: "",
-            },
-            message: "sai tên đăng nhập hoặc mật khẩu"
-          }
-        } else if(user.userActive === 0){
-          return {
-            code: 0,
-            data:{
-              codeErr: 'UNACTIVE',
-              accessToken: "",
-            },
-            message: "Tài khoản chưa kích hoạt."
-          }
-        } else if(user.userActive === 2){
-          return {
-            code: 0,
-            data:{
-              codeErr: 'LOCKED',
-              accessToken: "",
-            },
-            message: "Tài khoản bị khóa."
-          }
+    return null;
+  }
+
+  async sign(userDto: any) {
+    try {
+      const user = await this.validateUser(userDto.username, userDto.password);
+      if (user === null) {
+        return {
+          code: 0,
+          data: {
+            accessToken: "",
+          },
+          message: "sai tên đăng nhập hoặc mật khẩu"
         }
-        const payload = {
-          userCode: user.userCode,
-          userName: user.userName,
-          sub: user.userId,
-          userFullname: user.userFullname,
-          userPhonenumber: user.userPhonenumber,
+      } else if (user.userActive === 0) {
+        return {
+          code: 0,
+          data: {
+            codeErr: 'UNACTIVE',
+            accessToken: "",
+          },
+          message: "Tài khoản chưa kích hoạt."
+        }
+      } else if (user.userActive === 2) {
+        return {
+          code: 0,
+          data: {
+            codeErr: 'LOCKED',
+            accessToken: "",
+          },
+          message: "Tài khoản bị khóa."
+        }
+      }
+      const payload = {
+        userCode: user.userCode,
+        userName: user.userName,
+        sub: user.userId,
+        userFullname: user.userFullname,
+        userPhonenumber: user.userPhonenumber,
+      };
+      return {
+        code: 1,
+        data: {
+          accessToken: this.jwtService.sign(payload),
+          userFullname: payload.userFullname,
+          userPhonenumber: payload.userPhonenumber,
+        },
+        message: "Đăng nhập thành công!",
+      };
+    } catch (error) {
+      return {
+        code: 0,
+        data: {
+          accessToken: "",
+        },
+        message: error,
+      }
+    }
+  }
+
+  async checkToken(user: any) {
+    try {
+      const users = await this.usersRepository.findOne(user.userId);
+      if (users && users.userActive == 1)
+        return true;
+    } catch (err) {
+      console.log(err);
+    }
+    return false;
+  }
+
+  async changePassword(req: ChangePasswordDto): Promise<any> {
+    try {
+      const user = await this.validateUser(req.username, req.password);
+      if (user != null) {
+        //-----  UPDATE PASSWORD ------
+        user.userPassword = req.passwordNew;
+        await this.usersRepository.update(user.userId, user);
+        //const { userPassword, ...result } = user;
+        return {
+          code: 1,
+          msg: 'Thay đổi mật khẩu thành công!'
         };
+      } else {
+        return {
+          code: 0,
+          msg: 'Mật khẩu không chính xác!'
+        };
+      }
+    } catch (error) {
+      console.log(error);
+      return {
+        code: 0,
+        msg: 'Cập nhật mật khẩu thất bại!'
+      };
+    }
+
+  }
+
+  async findOne(id: string): Promise<any> {
+    try {
+      const user = await this.usersRepository.findOne(id);
+      if (!user) {
+        return {
+          code: 0,
+          data: null,
+          message: "Tài khoản không tồn tại."
+        }
+      } else {
+        return {
+          code: 1,
+          data: user,
+          message: "Tài khoản không tồn tại."
+        }
+      };
+    } catch (error) {
+      return {
+        code: 0,
+        data: null,
+        message: error,
+      }
+    }
+  }
+
+  async update(userId: string, usersDto: UsersDto) {
+    try {
+      const users = await this.usersRepository.findOne(userId);
+      if (!users) {
+        return {
+          code: 0,
+          data: null,
+          message: "Người dùng không tồn tại!"
+        }
+      } else {
+        users.userCode = usersDto.userCode.trim();
+        users.userFullname = usersDto.userFullname.trim();
+        users.userPhoneNumber = usersDto.userPhoneNumber;
+        users.userBirthday = new Date(usersDto.userBirthday.toString());
+        users.userGender = usersDto.userGender;
+        users.userAddress = usersDto.userAddress.trim();
+        users.userEmail = usersDto.userEmail.trim();
+        users.userAvatar = usersDto.userAvatar;
+        users.userActive = usersDto.userActive;
+        users.userUpdatedBy = usersDto.userUpdatedBy;
+        users.userUpdatedDate = new Date();
+
+        await this.usersRepository.update(userId, users);
+        return {
+          code: 1,
+          data: users,
+          message: "Cập nhật thành công!"
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      return {
+        code: 0,
+        data: null,
+        message: err,
+      }
+    }
+  }
+
+  async upload(avatar: AvatarDto, file: any) {
+    try {
+      const userId = avatar.avatarID;
+      const user = await this.usersRepository.findOne(userId);
+      if (user == null) {
+        // Delete the file
+        await unlinkAsync(file.path)
+        return {
+          code: 0,
+          data: null,
+          message: 'Avatar ID không tồn tại',
+        };
+      } else {
+        const oldUserAvatar = user.userAvatar;
+        // ------- UPDATE AVATAR PATH ------
+        user.userAvatar = AVT_PATH + file.filename;
+        await this.usersRepository.update(userId, user);
+        // Delete the file
+        await unlinkAsync(oldUserAvatar);
         return {
           code: 1,
           data: {
-            accessToken: this.jwtService.sign(payload),
-            userFullname: payload.userFullname,
-            userPhonenumber: payload.userPhonenumber,
-          },
-          message: "Đăng nhập thành công!",
-        };        
-      } catch (error) {
-        return {
-          code: 0,
-          data:{
-            accessToken: "",
-          },
-          message: error,
-        }
-      }
-    }
-
-    async checkToken(user: any)
-    {
-      try {
-        const users = await this.usersRepository.findOne(user.userId);
-        if(users && users.userActive == 1)
-          return true;
-      } catch (err) {
-        console.log(err);
-      }
-      return false;
-    }
-
-    async changePassword(req: ChangePasswordDto): Promise<any> {
-      try {
-        const user = await this.validateUser(req.username, req.password);
-        if (user != null) {
-          //-----  UPDATE PASSWORD ------
-          user.userPassword = req.passwordNew;
-          await this.usersRepository.update(user.userId, user);
-          //const { userPassword, ...result } = user;
-          return {
-            code: 1,
-            msg: 'Thay đổi mật khẩu thành công!'
-          };
-        } else {
-          return {
-            code: 0,
-            msg: 'Mật khẩu không chính xác!'
-          };
-        }
-      } catch (error) {
-        console.log(error);
-        return {
-          code: 0,
-          msg: 'Cập nhật mật khẩu thất bại!'
-        };
-      }
-
-    }
-
-    async findOne(id: string): Promise<any> {
-      try {
-        const user = await this.usersRepository.findOne(id);
-        if (!user) {
-          return {
-            code: 0,
-            data: null,
-            message: "Tài khoản không tồn tại."
-          }
-        }else{
-          return {
-            code: 1,
-            data: user,
-            message: "Tài khoản không tồn tại."
-          }
-        };
-      } catch (error) {
-        return {
-          code: 0,
-          data: null,
-          message: error,
-        }
-      }
-    }
-
-    async update(userId: string, usersDto: UsersDto) {
-      try {
-        const users = await this.usersRepository.findOne(userId);
-        if(!users){
-          return {
-            code: 0,
-            data:null,
-            message: "Người dùng không tồn tại!"
-          }
-        }else{
-          users.userCode = usersDto.userCode.trim();
-          users.userFullname = usersDto.userFullname.trim();
-          users.userPhoneNumber = usersDto.userPhoneNumber;
-          users.userBirthday = new Date(usersDto.userBirthday.toString());
-          users.userGender = usersDto.userGender;
-          users.userAddress = usersDto.userAddress.trim();
-          users.userEmail = usersDto.userEmail.trim();
-          users.userAvatar = usersDto.userAvatar;
-          users.userActive = usersDto.userActive;
-          users.userUpdatedBy =  usersDto.userUpdatedBy;
-          users.userUpdatedDate = new Date();
-  
-          await this.usersRepository.update(userId, users);
-          return {
-            code: 1,
-            data: users,
-            message: "Cập nhật thành công!"
-          }
-        }
-      } catch (err) {
-        console.log(err);
-        return {
-          code: 0,
-          data: null,
-          message: err,
-        }
-      }
-    }
-
-    async upload(avatar: AvatarDto, file: any) {
-      try {
-        const userId = avatar.avatarID;
-        // eslint-disable-next-line @typescript-eslint/no-this-alias
-        // const self = this;
-        const user = await this.usersRepository.findOne(userId);
-        if (user == null) {
-          return {
-            code: 0,
-            data:null,
-            message: 'Avatar ID không tồn tại',
-          };
-        };
-    
-        // const { originalname, size, buffer, mimetype } = file;
-        // const fileExtension: string = path.extname(originalname);
-        // const fileName: string = 'admin/users/avatar/' + userId + fileExtension;
-    
-        //let avtSrc: any = '';
-        // try {
-        //   const storage = multer.diskStorage({
-        //     destination: function (req, file, cb) {
-        //       cb(null, './uploads/avatars');
-        //     },
-        //     filename: function (req, file, cb) {
-        //       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        //       cb(null, file.fieldname + '-' + uniqueSuffix);
-        //       avtSrc = file.fieldname + '-' + uniqueSuffix;
-        //     }
-        //   })
-          
-        //   const upload = multer({ storage: storage })
-        //   console.log(upload);
-        // } catch (error) {
-        //   console.log(error)
-        // }
-    
-        // ------- UPDATE AVATAR PATH ------
-        //await this.usersRepository.update(userId, user);
-        // ------- GET URL -------
-        // try {
-        //   const url = await presignedGetObject(
-        //     fileName,
-        //   );
-    
-        //   avatar.avatarSrc = url;
-        // } catch (error) {
-        //   this.logger.error(error);
-        //   return error;
-        // }
-    
-        return {
-          code: 0,
-          data:{
-            avatarID: avatar,
-            avatarSrc: file.fieldname,
+            avatarID: userId,
+            avatarSrc: AVT_PATH + file.filename,
           },
           message: "Cập nhật avatar thành công"
         };
-      } catch (error) {
-        console.log(error);
-        return {
-          code: 0,
-          data:null,
-          message: error,
-        };
       }
+
+    } catch (error) {
+      console.log(error);
+      return {
+        code: 0,
+        data: null,
+        message: error,
+      };
     }
+  }
 }
 
